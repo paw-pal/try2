de52bb9f0c1fb787bc25959508f47dac15e05220
Ashley <a0958451329@yahoo.com.tw>
2023-06-02 17:52:34 +0800

    deploy

--- assets/NotFoundView-b8895542.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as o,o as n,c as a,b as r,w as s,F as c,a as _,d as l}from"./index-be27906c.js";const d={},i=_("h1",null,"404",-1);function u(m,f){const e=o("router-link");return n(),a(c,null,[i,r(e,{to:"/",class:"btn btn-primary"},{default:s(()=>[l("回首頁")]),_:1})],64)}const x=t(d,[["render",u]]);export{x as default};
